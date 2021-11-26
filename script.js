7202c2747e3adfc6389ae46a8bb01649f7871121
Daniyar Tule <daniyartule@gmail.com>
2021-11-26 23:05:11 +0300

    additional tasks

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 
-alert('Hello new js world');
-console.log("New JS student");
+let title = 'Домашнее задание'; //- строка с названием проекта,
+let screens = ("Простые, Сложные, Интерактивные");
+let screenPrice = 300; //- любое число,
+const rollback = 77; //- любое число от 1 до 100,
+let fullPrice = 1000; //- любое число (сколько хотите заработать),
+let adaptive = true; //- булевое значение
 
-
-var newVar = 10;
-let newSecondVar = "Var";
-const thisNewConstant = 10;
-
-console.log(newSecondVar);
-
-let title = "New var";
-let screens = 10;
-let screenPrice = 'Goga';
-const rollback = 56;
-const fullPrice = 555666;
-let adaptive = 'name';
-let newVarFor = 'this is just empty str';
-// new comment for mentor check
+console.log(typeof (title));
+console.log(typeof (fullPrice));
+console.log(typeof (adaptive));
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + (screenPrice) + " рублей/долларов/гривен/юани");
+console.log("Стоимость разработки сайта " + (fullPrice) + " рублей/долларов/гривен/юани");
+let screensNew = screens.toLowerCase();
+console.log(screensNew.split());
+console.log(fullPrice * (rollback / 100));
