cadca2b2fbfb0cb0c944025bf349109373ac5bdd
Daniyar Tule <daniyartule@gmail.com>
2021-11-26 14:42:28 +0300

    новая переменная для урока

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 alert('Hello new js world');
 console.log("New JS student");
+
+
 var newVar = 10;
 let newSecondVar = "Var";
 const thisNewConstant = 10;
@@ -13,3 +15,4 @@ let screenPrice = 'Goga';
 const rollback = 56;
 const fullPrice = 555666;
 let adaptive = 'name';
+let newVarFor = 'this is just empty str';
