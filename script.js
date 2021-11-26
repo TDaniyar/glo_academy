fe8581a93e9b45fa96a539be1b8e9ab7eabbcc70
Daniyar Tule <daniyartule@gmail.com>
2021-11-26 14:30:13 +0300

    только для первого урока

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ let newSecondVar = "Var";
 const thisNewConstant = 10;
 
 console.log(newSecondVar);
+
+let title = "New var";
+let screens = 10;
+let screenPrice = 'Goga';
+const rollback = 56;
+const fullPrice = 555666;
+let adaptive = 'name';
