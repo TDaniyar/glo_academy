d9e6d33096b9aa849913b36fd9e9dc27c08d92f1
Daniyar Tule <daniyartule@gmail.com>
2021-12-12 13:01:15 +0300

    lesson 11 only

--- Desktop/lesson11/script.js
+++ Desktop/lesson11/script.js
@@ -1,3 +1,26 @@
+'use strict';
 
-const click = document.querySelector('btn');
-console.log(click);
+const btn = document.getElementById("btn");
+const text = document.querySelector("[type=text]");
+const square = document.querySelector("#square");
+const circleButton = document.querySelector("#e_btn");
+const circle = document.querySelector("#circle");
+const range = document.getElementById('range');
+
+const buttonClick = function(event) {
+    const collor = function(element) {
+        square.style.backgroundColor = text.value;
+    };
+    btn.addEventListener('click', collor);
+
+};
+
+text.addEventListener("mousedown", buttonClick);
+circleButton.style.display = "none";
+
+const rangeChange = function(event){
+    circle.style.height = range.value;
+};
+range.addEventListener("input", rangeChange);
+range.addEventListener("change", rangeChange);
+console.dir(circle);
